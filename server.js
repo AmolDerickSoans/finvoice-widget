50ce306de6c71c793cc740a514615e8d75fa8097
AmolSoans <amol.soans@bajajfinserv.in>
2024-12-01 01:58:58 +0530

    added live server option for easier debugging

---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const path = require('path');
+const config = require('./webpack.config.js');
+
+// Create Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Modify webpack config for development server
+const webpackConfig = {
+  ...config,
+  entry: {
+    popup: ['webpack-hot-middleware/client?reload=true', './styles/tailwind.css', './public/popup/popup.js']
+  },
+  plugins: [
+    ...config.plugins,
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+// Create webpack compiler
+const compiler = webpack(webpackConfig);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Use webpack-dev-middleware
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: webpackConfig.output.publicPath || '/',
+  writeToDisk: true
+}));
+
+// Use webpack-hot-middleware
+app.use(webpackHotMiddleware(compiler));
+
+// Serve index.html for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'popup.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Development server running at http://localhost:${PORT}`);
+});
