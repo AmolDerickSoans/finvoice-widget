80a72d1aff50ad931c3ffbcc070e6f7be365434b
AmolSoans <amol.soans@bajajfinserv.in>
2024-12-08 14:46:32 +0530

    updated button

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -10,24 +10,29 @@ const Button = ({
 }) => {
   const progress = totalFields > 0 ? (validFields / totalFields) * 100 : 0;
   //const isEnabled = progress === 100;
-  const isEnabled = (type === 'reset'? true : (progress === 100))
+  const isEnabled = (type === 'reset' || 'update' ? true : (progress === 100))
 
   // Button type styles
   const typeStyles = {
     buy: {
-      enabled: "bg-green-600 hover:bg-green-600",
-      progress: "bg-green-600",
+      enabled: "hover:bg-green-600",
+      progress: "bg-green-500",
       text: "text-white"
     },
     Sell: {
-      enabled: "bg-red-600 hover:bg-red-600",
-      progress: "bg-red-600",
+      enabled: "hover:bg-red-600",
+      progress: "bg-red-500",
       text: "text-white"
     },
     reset: {
-      enabled: "bg-yellow-500 hover:bg-yellow-500",
-      progress: "bg-yelow-100",
+      enabled: "hover:bg-yellow-600",
+      progress: "bg-yelow-500",
       text: "text-black"
+    },
+    update: {
+      enabled: "hover:bg-purple-600",
+      progress: "bg-purple-500",
+      text: "text-white"
     }
   };
 
@@ -35,9 +40,10 @@ const Button = ({
 
   return (
     <div class={`relative w-full rounded-md overflow-hidden px-4 py-2 
-                 ${type === 'Sell' ? 'bg-red-100 focus:ring-red-800' : 
-                   type === 'reset' ? 'bg-yellow-500 focus:ring-yellow-800' : 
-                   'bg-green-100 focus:ring-green-800'}`}>
+                 ${type === 'Sell' ? 'bg-red-100' : 
+                   type === 'reset' ? 'bg-yellow-100 ' : 
+                   type === 'update' ? 'bg-purple-100 ' :
+                   'bg-green-100 '}`}>
       {/* Progress bar */}
       <div
         class={`absolute left-0 top-0 h-full transition-all duration-300 ease-out ${currentStyle.progress}`}
@@ -57,7 +63,7 @@ const Button = ({
           rounded-md
           ${isEnabled ? currentStyle.enabled : 'cursor-not-allowed'}
           ${currentStyle.text}
-          focus:outline-none focus:ring-2 focus:ring-offset-1
+          focus:outline-none focus:ring-1 focus:ring-offset-1
           ${type === 'buy' ? 'focus:ring-green-800' : type === 'Sell' ? 'focus:ring-red-800' : 'focus:ring-gray-800'}
         `}
       >
