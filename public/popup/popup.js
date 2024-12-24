4bf466cdbac6570728339b8434b32493387b25df
AmolSoans <amol.soans@bajajfinserv.in>
2024-12-25 00:41:45 +0530

    route updated with exit trade component

--- public/popup/popup.js
+++ public/popup/popup.js
@@ -9,6 +9,7 @@ import { TradeProvider } from '../../src/contexts/TradeContext.js';
 // Add CSS imports
 import '../../styles/tailwind.css'
 import UpdateTrade from '../../src/components/pages/UpdateTrade/UpdateTrade.js';
+import ExitTrade from '../../src/components/pages/ExitTrade.js';
 const App = () => {
   return (
   
@@ -20,6 +21,7 @@ const App = () => {
        <PostedTrades path="/"/>
        {/* <PostedTrades path="/posted-trades" /> Added second path */}
        <UpdateTrade path="/update-trade/:id" />
+       <ExitTrade path = "/exit-trade/:id" />
        <NewTradePage path="/new-trade" />
        </Router>
       </main>
